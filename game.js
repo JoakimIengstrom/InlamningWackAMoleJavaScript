482a542fb3f064e44d1a3f830c8a0689da583078
JoakimIEngstrom <joakim.engstrom@utb.ecutbildning.se>
2022-02-07 17:31:05 +0100

    Big refactor into logic and gluecode split

---
+++ game.js
@@ -0,0 +1,45 @@
+export class TimeManager {
+  constructor() {
+    this.lastHole;
+    this.timeLimit = 10000;
+    this.countdown = this.timeLimit / 1000;
+  }
+
+  countDown() {
+    this.countdown -= 1;
+  }
+
+  stillTimeLeft() {
+    if (this.countdown != 0);
+    return this.countdown;
+  }
+}
+
+export class GameScore {
+  constructor() {
+    this.score = 0;
+    this.newHighscore = 0;
+  }
+
+  whackedMole() {
+    this.score++;
+  }
+
+  saveHighScore() {
+    localStorage.setItem("save", JSON.stringify(this.newHighscore));
+  }
+  loadHighScore() {
+    let oldHighscore = localStorage.getItem("save");
+    if (localStorage.getItem("save") > 0) {
+      return oldHighscore;
+    } else {
+      oldHighscore = 0;
+      return oldHighscore;
+    }
+  }
+  uppdateHighScore() {
+    if (this.score > this.newHighscore) {
+      this.newHighscore = this.score;
+    }
+  }
+}
--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 //glue-code, (UI-code, frontend-code)
+
+import { TimeManager } from "./game.js";
+import { GameScore } from "./game.js";
+
+const logic = new TimeManager();
+const gameScore = new GameScore();
+
 const holes = document.querySelectorAll(".hole");
 const moles = document.querySelectorAll(".mole");
 const scoreBoard = document.querySelector(".score");
@@ -6,93 +13,80 @@ const highScore = document.querySelector(".highScore");
 const startButton = document.querySelector(".startButton");
 const countdownBoard = document.querySelector(".countdown");
 
-let lastHole;
-let score = 0;
-let countdown;
-let timeUp = false;
-let newHighscore = 0;
-let timeLimit = 20000;
-
-//--------------------------
-
-//random 0-5
-function pickRandomHole(holes) {
-  const randomHole = Math.floor(Math.random() * holes.length);
-  const hole = holes[randomHole];
-  //Check for not same hole
-  if (hole === lastHole) {
-    return pickRandomHole(holes);
-  }
-  lastHole = hole;
-  return hole;
-}
-
-function popOut() {
-  const time = Math.random() * 1300 + 400;
-  const hole = pickRandomHole(holes);
-  hole.classList.add("up");
-  setTimeout(function () {
-    hole.classList.remove("up");
-    if (!timeUp) popOut();
-  }, time);
-}
-
 function startGame() {
-  countdown = timeLimit / 1000;
   scoreBoard.textContent = 0;
   scoreBoard.style.textContent = "block";
   startButton.style.visibility = "hidden";
-  countdownBoard.textContent = "Time left: " + countdown;
-  timeUp = false;
-  score = 0;
+  countdownBoard.textContent = "Time left: " + logic.countdown;
   popOut();
-  setTimeout(function () {
-    timeUp = true;
-  }, timeLimit);
-
-  let startCountdown = setInterval(function () {
-    countdown -= 1;
-    countdownBoard.textContent = "Time left: " + countdown;
-    if (countdown < 0) {
-      countdown = 0;
-      clearInterval(startCountdown);
-      startButton.style.visibility = "visible";
-      countdownBoard.textContent = "Times up!";
-      if (score > newHighscore) {
-        newHighscore = score;
-      }
-
-      if (score > 0 && score == newHighscore) {
-        highScore.textContent = "New HighScore: " + score + " points";
-        localStorage.setItem("save", JSON.stringify(newHighscore));
-        if (score > 0 && score == newHighscore && score > 15) {
-          highScore.textContent = "Great job!: " + score;
-        }
-      }
-    }
-  }, 1000);
+  timer();
 }
 
-function printHighscore() {
-  let oldHighscore = localStorage.getItem("save");
-  if (localStorage.getItem("save") > 0) {
-    highScore.textContent = "HighScore: " + oldHighscore + " points";
+function popOut() {
+  const popOutTime = Math.random() * 1300 + 400;
+  const hole = pickRandomHole(holes);
+  hole.classList.add("up");
+
+  for (let mole of moles) {
+    if (mole.id.slice(-1) === hole.id.slice(-1)) {
+      mole.addEventListener("click", whackMole);
+
+      setTimeout(() => {
+        hole.classList.remove("up");
+        mole.removeEventListener("click", whackMole);
+        if (logic.stillTimeLeft()) popOut();
+      }, popOutTime);
+    }
   }
 }
 
-printHighscore();
+function pickRandomHole(holes) {
+  const randomHole = Math.floor(Math.random() * holes.length);
+  const hole = holes[randomHole];
+  if (hole === logic.lastHole) {
+    return pickRandomHole(holes);
+  }
+  logic.lastHole = hole;
+  return hole;
+}
 
-function whack(e) {
-  score++;
+function whackMole(e) {
+  gameScore.whackedMole();
   this.style.backgroundImage = 'url("/image/sonic3.png")';
   this.style.pointerEvents = "none";
   setTimeout(() => {
     this.style.backgroundImage = 'url("/image/sonic.png")';
     this.style.pointerEvents = "all";
   }, 800);
-  scoreBoard.textContent = score;
+  scoreBoard.textContent = gameScore.score;
 }
 
-startButton.addEventListener("click", startGame);
+function timer() {
+  let startCountdown = setInterval(() => {
+    logic.countDown();
+    countdownBoard.textContent = "Time left: " + logic.countdown;
+    if (logic.countdown <= 0) {
+      clearInterval(startCountdown);
+      startButton.style.visibility = "visible";
+      countdownBoard.textContent = "Times up!";
+      gameScore.uppdateHighScore();
+      scoreUppdate();
+    }
+  }, 1000);
+}
 
-moles.forEach((mole) => mole.addEventListener("click", whack));
+function scoreUppdate() {
+  if (gameScore.score > 0 && gameScore.score == gameScore.newHighscore) {
+    highScore.textContent = "New HighScore: " + gameScore.score + " points";
+    gameScore.saveHighScore();
+  }
+}
+
+function printHighscore() {
+  let printOutScore = gameScore.loadHighScore();
+  highScore.textContent = "HighScore: " + printOutScore + " points";
+}
+
+printHighscore();
+
+startButton.addEventListener("click", startGame);
